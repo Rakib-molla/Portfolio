22621849d89c494edcf49ee59202c66ce01e448f
rakib-molla <rakibmail1997@gmail.com>
2023-07-01 14:59:42 +0600

    portfolio added

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,15 +1,18 @@
 import portfolioImg01 from "../images/portfolio-01.png";
 import portfolioImg02 from "../images/portfolio-02.png";
+import portfolioImg03 from "../images/portfolio-03.png";
+import portfolioImg04 from "../images/portfolio-04.png";
+import portfolioImg05 from "../images/portfolio-05.png";
 
 
 const portfolios = [
   {
     id: "01",
     imgUrl: portfolioImg01,
-    category: "front-end",
+    category: "full-stack",
     title: "Sports Academy ",
     
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    technologies: ["React", "Tailwind css", "Firebase", "Node.js", "MongoDB"],
     siteUrl: "#",
     demo: "https://summer-camp-12.web.app/",
     client: "https://github.com/rakib-molla/summer-camp-client",
@@ -19,13 +22,46 @@ const portfolios = [
   {
     id: "02",
     imgUrl: portfolioImg02,
-    category: "Toy Marketplace",
+    category: "full-stack",
     title: "Toy MarketPlace",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    technologies: ["React", "Tailwind css", "Firebase", "Node.js", "MongoDB"],
     siteUrl: "#",
     demo: "https://toy-marketplace-assignment-11.web.app/",
     client: "https://github.com/rakib-molla/toy-market-client",
     server: "https://github.com/rakib-molla/toy-market-server",
+  },
+  {
+    id: "03",
+    imgUrl: portfolioImg03,
+    category: "front-end",
+    title: "Restaurant Website",
+    technologies: ["React", "Tailwind css", "Firebase", ],
+    siteUrl: "#",
+    demo: "https://chef-client-assignment-10.web.app/",
+    client: "https://github.com/rakib-molla/recipe-hunter-webiste-client",
+    server: "https://github.com/rakib-molla/recipe-hunter-webiste-server",
+  },
+  {
+    id: "04",
+    imgUrl: portfolioImg04,
+    category: "front-end",
+    title: "Restaurant Website",
+    technologies: ["React", "Tailwind css",],
+    siteUrl: "#",
+    demo: "https://fascinating-souffle-692e7e.netlify.app/",
+    client: "https://github.com/rakib-molla/knowledge-cafe-react-frontend",
+    server: "",
+  },
+  {
+    id: "05",
+    imgUrl: portfolioImg05,
+    category: "front-end",
+    title: "Restaurant Website",
+    technologies: ["HTML","CSS", "Tailwind css",],
+    siteUrl: "#",
+    demo: "https://benevolent-chebakia-879349.netlify.app/",
+    client: "",
+    server: "",
   },
  
 
